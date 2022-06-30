3fa2f303df664d63e25b51d6601006e2bed74513
Dinamitrii <k_karaivanov@abv.bg>
2022-06-30 12:08:16 +0300

    SoftUni Programming Basics with Python

--- loops_for_loop/number_sequence.py
+++ loops_for_loop/number_sequence.py
@@ -1,10 +1,11 @@
 import sys
+
 count_of_numbers = int(input())
 max_number = - sys.maxsize
 min_number = sys.maxsize
 for numbers in range(count_of_numbers):
     current_number = int(input())
-    if current_number>max_number:
+    if current_number > max_number:
         max_number = current_number
     if current_number < min_number:
         min_number = current_number
